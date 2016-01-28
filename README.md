931d8b2913d1af9f8c46fdf304a1026414c51dce
quxiaolong <quxiaolong@douban.com>
2016-01-28 12:25:15 +0800

    Init zeus

--- README.md
+++ README.md
@@ -1,7 +1,26 @@
 # Zeus
-###Uturboo电商平台
+### Uturboo 电商平台
 
 > 众神之王，奥林波斯的主宰，天空、天气、雷电、法律、秩序和命运之神。他是克洛诺斯和瑞亚的幼子，赫拉的丈夫。象征物是雄鹰、橡树、节棍、王座和山峰；他最爱的祭品是母山羊和牛角涂成金色的白色公牛。
 
+### Develop
+```
+make env                # init environment
+make data               # init database
+workon zenus            # use env
+./manage.py runserver   # run zenus
+```
 
-![440px-jupiter_smyrna_louvre_ma13](https://cloud.githubusercontent.com/assets/9249450/12633939/82fa3da6-c5b0-11e5-85c5-54863df178fc.jpg)
+### Commands
+```
+workon zeus             # enter dev env
+
+deactivate              # exit dev env
+
+make test               # run utiltest
+```
+
+### Version
+v0.0.1 Pre-alpha
+
+### Build Status
---
+++ manage.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import os
+
+from flask.ext.script import Manager, Server
+from flask.ext.script.commands import ShowUrls, Clean
+from zeus import create_app
+from zeus.models import db, User
+
+# default to dev config because no one should use this in
+# production anyway
+env = os.environ.get('zeus_ENV', 'dev')
+app = create_app('zeus.settings.%sConfig' % env.capitalize())
+manager = Manager(app)
+manager.add_command("server", Server())
+manager.add_command("show-urls", ShowUrls())
+manager.add_command("clean", Clean())
+
+
+@manager.shell
+def make_shell_context():
+    """ Creates a python REPL with several default imports
+        in the context of the app
+    """
+
+    return dict(app=app, db=db, User=User)
+
+
+@manager.command
+def createdb():
+    """ Creates a database with all of the tables defined in
+        your SQLAlchemy models
+    """
+
+    db.create_all()
+
+if __name__ == "__main__":
+    manager.run()
